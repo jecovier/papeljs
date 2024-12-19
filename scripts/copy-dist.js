e9b2c3edc123da3d50d073242dba3aa1d2a25327
jespinoza-sp <jespinoza@signpost.com>
2024-12-19 04:25:32 -0500

    esport distribution files

--- scripts/copy-dist.js
+++ scripts/copy-dist.js
@@ -3,17 +3,30 @@ import fs from "fs";
 console.log("Copying JS files...");
 // Source and destination paths
 const sourceFilePattern = "index-";
-const destinationFile = "dist/papel.js";
+const distributionFile = "dist/papel.js";
+const downloadFile = "docs/assets/papel.js";
 
 // Get the source file
 const sourceFile = fs
   .readdirSync("docs/assets")
-  .find((file) => file.startsWith(sourceFilePattern));
+  .find(
+    (file) => file.startsWith(sourceFilePattern) && file.slice(-3) === ".js"
+  );
 
-fs.copyFile("docs/assets/" + sourceFile, destinationFile, (err) => {
+// Copy the file to the distribution folder
+fs.copyFile("docs/assets/" + sourceFile, distributionFile, (err) => {
   if (err) {
     console.error("Error copying file:", err);
   } else {
-    console.log("File copied successfully:", destinationFile);
+    console.log("File copied successfully:", distributionFile);
+  }
+});
+
+// Copy the file to the download folder
+fs.copyFile("docs/assets/" + sourceFile, downloadFile, (err) => {
+  if (err) {
+    console.error("Error copying file:", err);
+  } else {
+    console.log("File copied successfully:", downloadFile);
   }
 });
