df625055a3fa9739734d4624d36c048085e43e08
jespinoza-sp <jespinoza@signpost.com>
2024-12-30 20:14:15 -0500

    change to data- attributes

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,11 +1,11 @@
-export const SLOT_ATTR_NAME = "pl-slot";
-export const PRESERVE_ATTR_NAME = "pl-preserve";
+export const SLOT_ATTR_NAME = "data-slot";
+export const PRESERVE_ATTR_NAME = "data-preserve";
 
-export const INTERCEPTED_ATTR_NAME = "pl-intercepted";
-export const NOT_INTERCEPTED_ATTR_NAME = "pl-intercepted";
+export const INTERCEPTED_ATTR_NAME = "data-intercepted";
+export const NOT_INTERCEPTED_ATTR_NAME = "data-nointercepted";
 
-export const PREFETCH_ATTR_NAME = "pl-prefetch";
-export const PREFETCHED_ATTR_NAME = "pl-prefetched";
+export const PREFETCH_ATTR_NAME = "data-prefetch";
+export const PREFETCHED_ATTR_NAME = "data-prefetched";
 
 export enum FetchMethod {
   "GET" = "GET",
